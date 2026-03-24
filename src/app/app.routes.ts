37caf6b3bb20d9cd2154d5c4bfab7128697da744
stalker9855 <andrey.l.evseev@gmail.com>
2026-03-24 17:42:59 +0200

    lr10

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,22 @@
 import { Routes } from '@angular/router';
-import { Wine } from './wine/wine';
-import { Greeting } from './greeting/greeting';
+import { Wine } from './components/wine/wine';
+import { Greeting } from './components/greeting/greeting';
+import { About } from './components/about/about';
+import { Help } from './components/help/help';
 
 export const routes: Routes = [
   {
     component: Greeting,
     path: '',
     pathMatch: 'full',
+  },
+  {
+    component: About,
+    path: 'about',
+  },
+  {
+    component: Help,
+    path: 'help',
   },
   {
     component: Wine,
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('lr-angular-project');
+  constructor(public router: Router) {}
 }
